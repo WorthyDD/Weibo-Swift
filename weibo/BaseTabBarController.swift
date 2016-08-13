af05e545a3cdfb2294d4891eaf9d523cd495ff3e
wuxi <duanwuxi@xxkuaipao.com>
2016-08-13 18:39:52 +0800

    s

---
+++ weibo/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  weibo
+//
+//  Created by 武淅 段 on 16/8/13.
+//  Copyright © 2016年 武淅 段. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    
+    override func viewDidLoad() {
+        
+    }
+}
